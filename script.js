ec25f3286b14f57bf74f10da3c01b97e73e236ae
gitbugi <oezdemirbu@web.de>
2022-11-21 15:50:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/*import { Typed } from 'typed.js';*/
+
+var typed = new Typed('.header-txt', {
+    strings: ["First sentence.", "Evolution der Angst", "Wann ist die Angst eine Krankheit ?", "Angststörungen"],
+    typeSpeed: 80,
+    backSpeed: 50,
+    loop: true
+});
