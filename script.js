4c416624058c64182294d849e903efb6687a64de
gitbugi <oezdemirbu@web.de>
2022-11-28 15:32:00 +0100

    Bilder hinzugefügt und neue Inhalt

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*import { Typed } from 'typed.js';*/
 
 var typed = new Typed('.header-txt', {
-    strings: ["First sentence.", "Evolution der Angst", "Wann ist die Angst eine Krankheit ?", "Angststörungen"],
+    strings: ["First sentence.", "Wann ist die Angst eine Krankheit ?", "Angststörungen"],
     typeSpeed: 80,
     backSpeed: 50,
     loop: true
